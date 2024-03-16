647784812259543921e876872c4ed0215e435818
Darek7D <darek7d@gmail.com>
2024-03-16 09:53:49 +0100

    Update readme and license (#8)

--- include/dsignal/min.h
+++ include/dsignal/min.h
@@ -1,23 +1,4 @@
-/*
- * Copyright (C) 2018 Tedtronix.
- * This file is part of DSignal.
- *
- * DSignal is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * DSignal is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with DSignal.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-#ifndef DSIGNAL_MIN_H
-#define DSIGNAL_MIN_H
+#pragma once
 
 #include "signalprocessorbuffered.h"
 #include "dsignal_export.h"
@@ -41,4 +22,3 @@ private:
 
 }
 
-#endif // DSIGNAL_MIN_H
